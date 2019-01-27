c6faa306f3e45d686fba07bd0f249f142945fbbc
mli <menghua.li@westjet.com>
2019-01-27 12:58:53 -0500

    Add geolocation support

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '..', 'public');
 
 var app = new express();
@@ -29,6 +29,10 @@ io.on('connection', (socket) => {
     // send message to all connected clients
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server');
+  });
+
+  socket.on('createLocationMessage', function(coords) {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate message', () => {
@@ -7,6 +7,18 @@ describe('generateMessage', () => {
     var text = 'Hello';
     var message = generateMessage(from, text);
     expect(message).toMatchObject({from, text});
+    expect(typeof message.createAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate location message', () => {
+    var from = 'Mike';
+    var latitude = '1234';
+    var longitude = '5678';
+    var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    var message = generateLocationMessage(from, latitude, longitude);
+    expect(message).toMatchObject({from, url});
     expect(typeof message.createAt).toBe('number');
   });
 });
